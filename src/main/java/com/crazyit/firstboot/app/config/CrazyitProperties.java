75a6616425f096fb93bfcc9aa8ea2e8c571e1626
liangjinyong <liangjy881@gmail.com>
2023-10-24 14:53:51 +0900

    补充提交遗漏的文件

--- src/main/java/com/crazyit/firstboot/app/config/CrazyitProperties.java
+++ src/main/java/com/crazyit/firstboot/app/config/CrazyitProperties.java
@@ -1,2 +1,68 @@
-package com.crazyit.firstboot.app.config;public class CrazyitProperties {
+package com.crazyit.firstboot.app.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// 指定读取以org.crazyit开头的属性
+@ConfigurationProperties(prefix = "org.crazyit", ignoreInvalidFields = false)
+@Component
+public class CrazyitProperties {
+    private boolean enabled;
+    private String name;
+    private InetAddress remoteAddress;
+    private final Item item = new Item();
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public InetAddress getRemoteAddress() {
+        return remoteAddress;
+    }
+
+    public void setRemoteAddress(InetAddress remoteAddress) {
+        this.remoteAddress = remoteAddress;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public static class Item {
+        private String brand;
+        private List<String> comments = new ArrayList<>(Collections.singleton("GREAT"));
+
+        public String getBrand() {
+            return brand;
+        }
+
+        public void setBrand(String brand) {
+            this.brand = brand;
+        }
+
+        public List<String> getComments() {
+            return comments;
+        }
+
+        public void setComments(List<String> comments) {
+            this.comments = comments;
+        }
+    }
 }
--- src/main/java/com/crazyit/firstboot/ch02/Ch02Controller.java
+++ src/main/java/com/crazyit/firstboot/ch02/Ch02Controller.java
@@ -1,2 +1,25 @@
-package com.crazyit.firstboot.ch02;public class Ch02Controller {
+package com.crazyit.firstboot.ch02;
+
+import com.crazyit.firstboot.app.config.CrazyitProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ch02")
+public class Ch02Controller {
+    private final CrazyitProperties crazyitProperties;
+
+    // 依赖注入CrazyitProperties属性处理Bean
+    @Autowired
+    public Ch02Controller(CrazyitProperties crazyitProperties) {
+        this.crazyitProperties = crazyitProperties;
+    }
+
+    @GetMapping("configProps")
+    public CrazyitProperties configProps() {
+        return crazyitProperties;
+    }
+
 }
