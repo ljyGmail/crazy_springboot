fd8eac948ed6d5aefd483a734e3e572eb0080cf8
liangjinyong <liangjy881@gmail.com>
2023-10-28 13:57:11 +0900

    2.4.2 添加活动Profile

--- src/main/java/com/crazyit/firstboot/FirstbootApplication.java
+++ src/main/java/com/crazyit/firstboot/FirstbootApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        var app = new SpringApplication(FirstbootApplication.class);
+        // 添加Profile
+        app.setAdditionalProfiles("addition");
+        app.run(args);
+    }
 }
